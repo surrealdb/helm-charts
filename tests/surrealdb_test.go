d196aaf732ade2a68092652f811ae624063cfff8
Yusuke Kuoka <ykuoka@gmail.com>
2024-10-28 12:24:58 +0000

    Add deployment.yaml snapshots with bugs anyway

--- tests/surrealdb_test.go
+++ tests/surrealdb_test.go
@@ -4,4 +4,12 @@ import "testing"
 
 func TestDeployment(t *testing.T) {
 	testTemplate(t, "deployment.yaml", "default", map[string]interface{}{})
+	testTemplate(t, "deployment.yaml", "disable auth using deprecated surrealdb.auth", map[string]interface{}{
+		"surrealdb": map[string]interface{}{
+			"auth": false,
+		},
+	})
+	testTemplate(t, "deployment.yaml", "disable auth using surrealdb.unauhenticated", map[string]interface{}{
+		"unauthenticated": true,
+	})
 }

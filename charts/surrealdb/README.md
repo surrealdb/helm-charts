3064fe914094513a9516fa120c948effc46ee479
Danila <beolnix@gmail.com>
2024-11-12 11:50:05 +0100

    migration to 2.0: replace SURREAL_AUTH env var with SURREAL_UNAUTHENTICATED (#9)
    Co-authored-by: Gerard Guillemas Martos <gguillemas@users.noreply.github.com>
    Co-authored-by: Gerard Guillemas Martos <gerard.guillemas@surrealdb.com>
    Co-authored-by: Yusuke Kuoka <ykuoka@gmail.com>

--- charts/surrealdb/README.md
+++ charts/surrealdb/README.md
@@ -1,6 +1,6 @@
 # SurrealDB Helm Chart
 
-![Version: 0.3.4](https://img.shields.io/badge/Version-0.3.4-informational?style=flat-square) ![Type: application](https://img.shields.io/badge/Type-application-informational?style=flat-square) ![AppVersion: 1.0.0](https://img.shields.io/badge/AppVersion-1.0.0-informational?style=flat-square)
+![Version: 0.3.5](https://img.shields.io/badge/Version-0.3.5-informational?style=flat-square) ![Type: application](https://img.shields.io/badge/Type-application-informational?style=flat-square) ![AppVersion: 1.0.0](https://img.shields.io/badge/AppVersion-1.0.0-informational?style=flat-square)
 
 SurrealDB is the ultimate cloud database for tomorrow's applications.
 
--- tests/surrealdb_test.go
+++ tests/surrealdb_test.go
@@ -4,4 +4,24 @@ import "testing"
 
 func TestDeployment(t *testing.T) {
 	testTemplate(t, "deployment.yaml", "default", map[string]interface{}{})
+	testTemplate(t, "deployment.yaml", "release deployed using pre v0.3.5 chart with the default surrealdb.auth", map[string]interface{}{
+		"surrealdb": map[string]interface{}{
+			"auth": true,
+		},
+	})
+	testTemplate(t, "deployment.yaml", "disable auth using deprecated surrealdb.auth", map[string]interface{}{
+		"surrealdb": map[string]interface{}{
+			"auth": false,
+		},
+	})
+	testTemplate(t, "deployment.yaml", "surrealdb.unauthenticated=false has no effect", map[string]interface{}{
+		"surrealdb": map[string]interface{}{
+			"unauthenticated": false,
+		},
+	})
+	testTemplate(t, "deployment.yaml", "disable auth using surrealdb.unauhenticated", map[string]interface{}{
+		"surrealdb": map[string]interface{}{
+			"unauthenticated": true,
+		},
+	})
 }

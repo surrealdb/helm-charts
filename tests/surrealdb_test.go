9cc24cfb540e7a4ec36cea6d5d38a67560e8b60f
Yusuke Kuoka <yusuke.kuoka@surrealdb.com>
2025-09-02 13:27:07 +0900

    Update test snapshots (#24)

--- tests/surrealdb_test.go
+++ tests/surrealdb_test.go
@@ -41,4 +41,28 @@ func TestDeployment(t *testing.T) {
 			},
 		},
 	})
+	testTemplate(t, "deployment.yaml", "init and extra containers are set", map[string]interface{}{
+		"initContainers": []interface{}{
+			map[string]interface{}{
+				"name":  "init-myservice",
+				"image": "myservice:latest",
+				"command": []string{
+					"/bin/sh",
+					"-c",
+					"echo Init Container",
+				},
+			},
+		},
+		"extraContainers": []interface{}{
+			map[string]interface{}{
+				"name":  "extra-myservice",
+				"image": "myservice:latest",
+				"command": []string{
+					"/bin/sh",
+					"-c",
+					"echo Extra Container",
+				},
+			},
+		},
+	})
 }
